0448bfbcd22b2eda2ac4abb9a206365b8e7d01d1
杨象军 <yangxiangjun@yy.com>
2019-05-01 18:16:43 +0800

    第四道LeetCode Median of Two Sorted Arrays

--- practice/Leetcode04.go
+++ practice/Leetcode04.go
@@ -52,6 +52,6 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
 func main(){
 	nums1 := []int{1,2}
-	nums2 := []int{3,4}
+	nums2 := []int{3,4,5}
 	fmt.Println(findMedianSortedArrays(nums1,nums2))
 }
